b2d204505e871b7b0f29b428d00288f4bb12e14a
VARAD87 <varadnangare87@gmail.com>
2026-09-18 23:44:10 +0530

    Update plot_training_curves.py

--- src/visualization/plot_training_curves.py
+++ src/visualization/plot_training_curves.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-# Values from training output (Day 6)
+
 epochs = [1, 2, 3, 4, 5]
 
 train_loss = [0.7791, 0.4466, 0.3845, 0.2924, 0.2501]
@@ -9,7 +9,7 @@ val_loss   = [0.5371, 0.4137, 0.3575, 0.2878, 0.2323]
 train_acc = [0.7286, 0.8251, 0.8578, 0.8857, 0.9009]
 val_acc   = [0.7796, 0.8429, 0.8497, 0.8825, 0.9108]
 
-# Loss plot
+
 plt.figure()
 plt.plot(epochs, train_loss, label="Train Loss")
 plt.plot(epochs, val_loss, label="Validation Loss")
@@ -21,7 +21,7 @@ plt.grid()
 plt.savefig("reports/figures/loss_curve.png", dpi=300)
 plt.show()
 
-# Accuracy plot
+
 plt.figure()
 plt.plot(epochs, train_acc, label="Train Accuracy")
 plt.plot(epochs, val_acc, label="Validation Accuracy")
